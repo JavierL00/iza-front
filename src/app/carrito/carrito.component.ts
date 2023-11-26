8a3761a8102abe948dfea6606bdc64d6425c4b10
Javier Luján <javier.lujan.121@outlook.es>
2023-11-26 11:00:27 -0500

    set swal

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
 interface CartItem {
   nombre: string;
   cantidad: number;
@@ -7,36 +9,36 @@ interface CartItem {
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
-  styleUrls: ['./carrito.component.css']
+  styleUrls: ['./carrito.component.css'],
 })
 export class CarritoComponent {
-  cartItems:any;
+  cartItems: any;
   constructor() {
     this.cartItems = [
       {
-        "nombre": "Roll screen",
-        "cantidad":1,
-        "precio": 50.00
+        nombre: 'Roll screen',
+        cantidad: 1,
+        precio: 50.0,
       },
       {
-        "nombre": "Folder Corporativo",
-        "cantidad":1,
-        "precio": 12
+        nombre: 'Folder Corporativo',
+        cantidad: 1,
+        precio: 12,
       },
       {
-        "nombre": "Bolsa (ecologica)",
-        "cantidad":1,
-        "precio": 4
+        nombre: 'Bolsa (ecologica)',
+        cantidad: 1,
+        precio: 4,
       },
       {
-        "nombre": "Bolsa (personalizada)",
-        "cantidad":1,
-        "precio": 6
+        nombre: 'Bolsa (personalizada)',
+        cantidad: 1,
+        precio: 6,
       },
       {
-        "nombre": "Lapiceros Ecologicos",
-        "cantidad":1,
-        "precio": 2
+        nombre: 'Lapiceros Ecologicos',
+        cantidad: 1,
+        precio: 2,
       },
     ];
   }
@@ -52,6 +54,29 @@ export class CarritoComponent {
   }
 
   calculateTotal(): number {
-    return this.cartItems.reduce((total: number, item: CartItem) => total + (item.precio * item.cantidad), 0);
+    return this.cartItems.reduce(
+      (total: number, item: CartItem) => total + item.precio * item.cantidad,
+      0
+    );
+  }
+
+  payButton() {
+    Swal.fire({
+      title: '¿Estas seguro que deseas realizar la compra?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#4caf50',
+      cancelButtonColor: '#f44336',
+      confirmButtonText: 'Si, comprar!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Compra realizada!',
+          'Tu compra se realizo correctamente.',
+          'success'
+        );
+      }
+    });
   }
 }
