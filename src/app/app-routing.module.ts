bce39551f90a348d61b3bba423e1fc23801964d9
mosho910 <mosho9100@gmail.com>
2023-11-25 20:34:57 -0500

    pantallas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./extranet/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  // Puedes agregar más rutas aquí según sea necesario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/extranet/login/login.component.ts
+++ src/app/extranet/login/login.component.ts
@@ -1,3 +1,5 @@
+// login.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,7 +12,7 @@ export class LoginComponent {
   password: string = '';
 
   login() {
-    // Aquí puedes implementar la lógica de inicio de sesión
+    // Resto del código de inicio de sesión
     console.log('Usuario:', this.username);
     console.log('Contraseña:', this.password);
     // Lógica adicional...
