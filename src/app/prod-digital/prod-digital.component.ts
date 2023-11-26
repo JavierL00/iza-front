93db8110816f72c68ac047bff546d1537a5ef9c2
Javier Luján <javier.lujan.121@outlook.es>
2023-11-26 10:33:36 -0500

    productos

--- src/app/prod-digital/prod-digital.component.ts
+++ src/app/prod-digital/prod-digital.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdDigitalService } from './prod-digital.service';
+
 
 @Component({
   selector: 'app-prod-digital',
   templateUrl: './prod-digital.component.html',
   styleUrls: ['./prod-digital.component.css']
 })
-export class ProdDigitalComponent {
+export class ProdDigitalComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProdDigitalService) {}
+
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
